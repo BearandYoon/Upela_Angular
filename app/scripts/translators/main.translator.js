ca5058755e686f4efba362bf38079be8facce2a9
bear <luckysevenbear@gmail.com>
2016-11-06 18:58:31 +0800

    added main-translator

--- app/scripts/translators/main.translator.js
+++ app/scripts/translators/main.translator.js
@@ -7,49 +7,286 @@
 
   function MainTranslate ($translateProvider) {
     $translateProvider.translations('en', {
-      'MAIN': {
-        'TITLE': 'Upela_en',
-        'MENU': {
-          'PROFESSIONALS': 'Professionals',
-          'SERVICES': 'Services',
-          'TRACKING': 'Tracking',
-          'HELP': 'Help',
-          'BLOG': 'Blog',
-          'CONTACT': 'Contact',
-          'LOGIN': 'Login',
-          'REGISTER': 'Register'
+      MAIN: {
+        TITLE: 'Upela',
+        MENU: {
+          PROFESSIONALS: 'Professionals',
+          SERVICES: {
+            TITLE: 'Services',
+            DESTINATIONS: {
+              TITLE: 'Destinations',
+              AFRICA: 'Africa',
+              AMERICA: 'America',
+              ASIA: 'Asia',
+              DOM_TOM: 'DOM-TOM',
+              EASTERN_EUROPE: 'Eastern Europe',
+              EUROPE: 'Europe',
+              MIDDLE_EAST: 'Middle East'
+            },
+            EXPRESS_COURIER: {
+              TITLE: 'Express courier',
+              FRANCE: 'France',
+              EUROPE: 'Europe',
+              AIX: 'Aix en Provence',
+              BORDEAUX: 'Bordeaux',
+              LILLE: 'Lille',
+              LYON: 'Lyon',
+              MARSEILLE: 'Marseille',
+              MONTPELLIER: 'Montpellier',
+              NANTES: 'Nantes',
+              NICE: 'Nice',
+              PARIS: 'Paris',
+              RENNES: 'Rennes',
+              STRASBOURG: 'Strabourg',
+              TOULOUSE: 'Toulouse'
+            },
+            CARRIERS: {
+              TITLE: 'Carriers',
+              CHRONOPOST: 'Chronopost',
+              COLISSIMO: 'Colissimo',
+              DHL: 'DHL',
+              DPD: 'DPD',
+              FEDEX: 'FedEx',
+              MONDIAL: 'Mondial Relay',
+              TNT: 'TNT',
+              UPS: 'UPS'
+            }
+          },
+          TRACKING: 'Tracking',
+          HELP: 'Help',
+          BLOG: 'Blog',
+          CONTACT: 'Contact',
+          LOGIN: 'Login',
+          REGISTER: 'Register'
+        },
+        PAGES: {
+          TITLE: 'Upela: compare quotes from carriers for the delivery and tracking of your consignments, parcels or pallets for professionals or individuals',
+          PAGE_ONE: {
+            E_MERCHANTS: 'E-merchants',
+            TEXT: 'Centralize all your sendings<br>Save time, avoid mistakes'
+          },
+          PAGE_TWO: {
+            PRO: 'N°1 for the PROs',
+            TEXT: '+20,000 customers<br> Immediate discounts <br> Deferred payment by Sepa'
+          },
+          PAGE_THREE: {
+            TITLE1: 'Delivery Day+1 ',
+            TITLE2: 'More than 220 destinations',
+            EUROPE: 'Europe',
+            USA: 'USA',
+            MIDDLE_EAST: 'Middle East',
+            AFRICA: 'Africa',
+            ASIA: 'Asia',
+            FOOTER: {
+              TITLE: 'Public rates tax inc.',
+              LINK: 'PRO rate?',
+              FOOTER: '(* from)'
+            }
+          },
+          PAGE_FOUR: {
+            TITLE: 'Simplify your exports <br>with the express shipping leaders<br>at the lowest prices',
+            OPTION1: 'Door-to-Door Delivery',
+            OPTION2: 'Customs service included',
+            OPTION3: 'Personality Tip'
+          },
+          PAGE_FIVE: {
+            TITLE: 'Why the PROs<br>choose us?',
+            OPTION1: 'to simplify their deliveries',
+            OPTION2: 'to gain time',
+            OPTION3: 'for our rates',
+            OPTION4: 'for our experience',
+            OPTION5: 'for our destinations'
+          }
         }
       }
     });
 
     $translateProvider.translations('fr', {
-      'MAIN': {
-        'TITLE': 'Upela_fr',
-        'MENU': {
-          'PROFESSIONALS': 'Professionals',
-          'SERVICES': 'Services',
-          'TRACKING': 'Tracking',
-          'HELP': 'Help',
-          'BLOG': 'Blog',
-          'CONTACT': 'Contact',
-          'LOGIN': 'Login',
-          'REGISTER': 'Register'
+      MAIN: {
+        TITLE: 'Upela',
+        MENU: {
+          PROFESSIONALS: 'Professionals',
+          SERVICES: {
+            TITLE: 'Services',
+            DESTINATIONS: {
+              TITLE: 'Destinations',
+              AFRICA: 'Afrique',
+              AMERICA: 'Amérique',
+              ASIA: 'Asie',
+              DOM_TOM: 'DOM-TOM',
+              EASTERN_EUROPE: 'Europe de l\'Est',
+              EUROPE: 'Europe',
+              MIDDLE_EAST: 'Moyen Orient'
+            },
+            EXPRESS_COURIER: {
+              TITLE: 'Service coursier',
+              FRANCE: 'France',
+              EUROPE: 'Europe',
+              AIX: 'Aix en Provence',
+              BORDEAUX: 'Bordeaux',
+              LILLE: 'Lille',
+              LYON: 'Lyon',
+              MARSEILLE: 'Marseille',
+              MONTPELLIER: 'Montpellier',
+              NANTES: 'Nantes',
+              NICE: 'Nice',
+              PARIS: 'Paris',
+              RENNES: 'Rennes',
+              STRASBOURG: 'Strabourg',
+              TOULOUSE: 'Toulouse'
+            },
+            CARRIERS: {
+              TITLE: 'Carriers',
+              CHRONOPOST: 'Chronopost',
+              COLISSIMO: 'Colissimo',
+              DHL: 'DHL',
+              DPD: 'DPD',
+              FEDEX: 'FedEx',
+              MONDIAL: 'Mondial Relay',
+              TNT: 'TNT',
+              UPS: 'UPS'
+            }
+          },
+          TRACKING: 'Suivi',
+          HELP: 'Assistance',
+          BLOG: 'Blog',
+          CONTACT: 'Contact',
+          LOGIN: 'Se connecter',
+          REGISTER: 'Inscription'
+        },
+        PAGES: {
+          TITLE: 'Upela : comparez les offres de transporteurs pour la livraison et le suivi de vos envois, colis ou palettes pour professionnels ou particuliers',
+          PAGE_ONE: {
+            E_MERCHANTS: 'E-commerçants',
+            TEXT: 'Centralisez toutes vos expéditions<br>Gagnez du temps, évitez les erreurs'
+          },
+          PAGE_TWO: {
+            PRO: 'N°1 chez les PRO',
+            TEXT: '+20,000 clients<br> immédiate Remises <br> Paiement différé by Sepa'
+          },
+          PAGE_THREE: {
+            TITLE1: 'Delivery Day+1 ',
+            TITLE2: 'More than 220 destinations',
+            EUROPE: 'Europe',
+            USA: 'USA',
+            MIDDLE_EAST: 'Middle East',
+            AFRICA: 'Africa',
+            ASIA: 'Asia',
+            FOOTER: {
+              TITLE: 'Public rates tax inc.',
+              LINK: 'PRO rate?',
+              FOOTER: '(* from)'
+            }
+          },
+          PAGE_FOUR: {
+            TITLE: 'Simplify your exports <br>with the express shipping leaders<br>at the lowest prices',
+            OPTION1: 'Door-to-Door Delivery',
+            OPTION2: 'Customs service included',
+            OPTION3: 'Personality Tip'
+          },
+          PAGE_FIVE: {
+            TITLE: 'Why the PROs<br>choose us?',
+            OPTION1: 'to simplify their deliveries',
+            OPTION2: 'to gain time',
+            OPTION3: 'for our rates',
+            OPTION4: 'for our experience',
+            OPTION5: 'for our destinations'
+          }
         }
       }
     });
 
     $translateProvider.translations('es', {
-      'MAIN': {
-        'TITLE': 'Upela_es',
-        'MENU': {
-          'PROFESSIONALS': 'Professionals',
-          'SERVICES': 'Services',
-          'TRACKING': 'Tracking',
-          'HELP': 'Help',
-          'BLOG': 'Blog',
-          'CONTACT': 'Contact',
-          'LOGIN': 'Login',
-          'REGISTER': 'Register'
+      MAIN: {
+        TITLE: 'Upela',
+        MENU: {
+          PROFESSIONALS: 'Professionals',
+          SERVICES: {
+            TITLE: 'Services',
+            DESTINATIONS: {
+              TITLE: 'Destinations',
+              AFRICA: 'Africa',
+              AMERICA: 'America',
+              ASIA: 'Asia',
+              DOM_TOM: 'DOM-TOM',
+              EASTERN_EUROPE: 'Eastern Europe',
+              EUROPE: 'Europe',
+              MIDDLE_EAST: 'Middle East'
+            },
+            EXPRESS_COURIER: {
+              TITLE: 'Express courier',
+              FRANCE: 'France',
+              EUROPE: 'Europe',
+              AIX: 'Aix en Provence',
+              BORDEAUX: 'Bordeaux',
+              LILLE: 'Lille',
+              LYON: 'Lyon',
+              MARSEILLE: 'Marseille',
+              MONTPELLIER: 'Montpellier',
+              NANTES: 'Nantes',
+              NICE: 'Nice',
+              PARIS: 'Paris',
+              RENNES: 'Rennes',
+              STRASBOURG: 'Strabourg',
+              TOULOUSE: 'Toulouse'
+            },
+            CARRIERS: {
+              TITLE: 'Carriers',
+              CHRONOPOST: 'Chronopost',
+              COLISSIMO: 'Colissimo',
+              DHL: 'DHL',
+              DPD: 'DPD',
+              FEDEX: 'FedEx',
+              MONDIAL: 'Mondial Relay',
+              TNT: 'TNT',
+              UPS: 'UPS'
+            }
+          },
+          TRACKING: 'Tracking',
+          HELP: 'Help',
+          BLOG: 'Blog',
+          CONTACT: 'Contact',
+          LOGIN: 'Login',
+          REGISTER: 'Register'
+        },
+        PAGES: {
+          TITLE: 'Upela: compare quotes from carriers for the delivery and tracking of your consignments, parcels or pallets for professionals or individuals',
+          PAGE_ONE: {
+            E_MERCHANTS: 'E-merchants',
+            TEXT: 'Centralize all your sendings<br>Save time, avoid mistakes'
+          },
+          PAGE_TWO: {
+            PRO: 'N°1 for the PROs',
+            TEXT: '+20,000 customers<br> Immediate discounts <br> Deferred payment by Sepa'
+          },
+          PAGE_THREE: {
+            TITLE1: 'Delivery Day+1 ',
+            TITLE2: 'More than 220 destinations',
+            EUROPE: 'Europe',
+            USA: 'USA',
+            MIDDLE_EAST: 'Middle East',
+            AFRICA: 'Africa',
+            ASIA: 'Asia',
+            FOOTER: {
+              TITLE: 'Public rates tax inc.',
+              LINK: 'PRO rate?',
+              FOOTER: '(* from)'
+            }
+          },
+          PAGE_FOUR: {
+            TITLE: 'Simplify your exports <br>with the express shipping leaders<br>at the lowest prices',
+            OPTION1: 'Door-to-Door Delivery',
+            OPTION2: 'Customs service included',
+            OPTION3: 'Personality Tip'
+          },
+          PAGE_FIVE: {
+            TITLE: 'Why the PROs<br>choose us?',
+            OPTION1: 'to simplify their deliveries',
+            OPTION2: 'to gain time',
+            OPTION3: 'for our rates',
+            OPTION4: 'for our experience',
+            OPTION5: 'for our destinations'
+          }
         }
       }
     });
