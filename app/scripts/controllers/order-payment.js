214de841c74ce7b9960c564a1bb4c82ae86ec5ea
bear <luckysevenbear@gmail.com>
2016-11-27 21:32:42 +0100

    integrate waybill endpoint

--- app/scripts/controllers/order-payment.js
+++ app/scripts/controllers/order-payment.js
@@ -8,8 +8,27 @@
  * Controller of the UpelaApp
  */
 angular.module('UpelaApp')
-  .controller('OrderPaymentCtrl', function ($scope, $state) {
-    $scope.clickPayment = function() {
-      $state.go('my-account');
+  .controller('OrderPaymentCtrl', function ($scope, $state, $stateParams, MainService) {
+    var vm = this;
+    vm.offers = $stateParams.offers;
+    vm.origin_shipment = $stateParams.shipment;
+    vm.offer_id = $stateParams.offer_id;
+    vm.waybill = $stateParams.waybill;
+    console.log('OrderPaymentCtrl = ', vm.waybill);
+
+    if(!vm.offers || !vm.origin_shipment || !vm.waybill || vm.offer_id) {
+      $state.go('order-order', {shipment: vm.origin_shipment, offers: vm.offers, offer_id: vm.offer_id});
+    }
+
+    vm.remakeShipment = function() {
+      $state.go('home', {shipment: vm.origin_shipment});
+    };
+
+    vm.remakeOffer = function() {
+      $state.go('order-offer', {shipment: vm.origin_shipment, offers: vm.offers});
+    };
+
+    vm.remakeOrder = function() {
+      $state.go('order-order', {shipment: vm.origin_shipment, offers: vm.offers, offer_id: vm.offer_id});
     };
    });
